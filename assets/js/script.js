57313d1e196f788d56143bd71defb21bfe5b8162
kbliss86 <132328278+kbliss86@users.noreply.github.com>
2023-06-22 00:59:33 -0500

    adding style code and made adjustments to HTML to accomodate timer JS and other future JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
-//Code Start
+// variables
+var questionEl = document.getElementById("question-display")
+//timer variables
+var timerEl = document.getElementById("timer")
+var secondsLeft = 10;
+var secondsSubtract = 0;
 
-var helloWorld = "";
+function setTime() {
+    var timerInterval =setInterval(function() {
+        secondsLeft--;
+        timerEl.textContent = "Time Remaining: " + secondsLeft;
+
+        if(secondsLeft ===0) {
+            clearInterval(timerInterval);
+            sendMessage();
+        }
+    }, 1000)
+}
+
+function sendMessage() {
+    questionEl.textContent = "Times up! You Lose!";
+}
+
+setTime();
